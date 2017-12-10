490045a7d4c3f6b762b654d3941c4d42f883296c
Unknown <matthewrmoyer@gmail.com>
2017-12-10 13:08:36 -0700

    separate store into multiple files actions mutations etc

---
+++ src/store/getters.js
@@ -0,0 +1,4 @@
+
+export const doubleCounter = (state) => state.counter * 2
+export const stringCounter = (state) => state.counter + ' clicks'
+export const value = (state) => state.value
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import * as getters from './getters'
+import * as actions from './actions'
+import * as mutations from './mutations'
+
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
@@ -8,37 +12,7 @@ export const store = new Vuex.Store({
         counter: 0,
         value: 0
     },
-    getters : {
-      doubleCounter: state => state.counter * 2,
-      stringCounter: state => state.counter + ' clicks',
-      value: state => state.value
-    },
-    mutations: {
-        increment: state => state.counter++,
-        decrement: state => state.counter--,
-        asyncIncrementBy: (state, payload) => {
-            state.counter += payload.by
-        },
-        updateValue: (state, payload) => {
-            state.value = payload
-        }
-    },
-    actions: {
-        increment: context => context.commit('increment'),
-        asyncIncrementBy: ( {commit}, payload) => {
-            setTimeout(() => {
-                commit('asyncIncrementBy', payload)
-            }, payload.duration);
-        },
-
-        decrement: context => context.commit('decrement'),
-        asyncDecrement: ({ commit }) => {
-            setTimeout(() => {
-                commit('decrement')
-            }, 1000);
-        },
-        updateValue: ({ commit }, payload) => {
-            commit('updateValue', payload)
-        }
-    }
+    getters,
+    mutations,
+    actions
 })
