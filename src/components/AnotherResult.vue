e9b831a121bb56c639d0b3c83fe858de1560cca1
Unknown <matthewrmoyer@gmail.com>
2017-12-09 23:12:23 -0700

    use getters

---
+++ src/components/AnotherResult.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Another Result</h1>
+        <p>Another counter is {{ doubleCounter }}</p>
+        <p>{{ stringCounter }} </p>
+        
+
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+    export default {
+        computed: {
+            ...mapGetters([
+                'doubleCounter',
+                'stringCounter'
+            ])
+        }
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
