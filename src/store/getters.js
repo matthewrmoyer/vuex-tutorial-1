6009c84327291da7832a33481ccef3f85b3f36bc
Unknown <matthewrmoyer@gmail.com>
2017-12-10 13:39:13 -0700

    single file module for counter

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,1 @@
-
-export const doubleCounter = (state) => state.counter * 2
-export const stringCounter = (state) => state.counter + ' clicks'
 export const value = (state) => state.value
--- src/store/store.js
+++ src/store/store.js
@@ -4,15 +4,18 @@ import Vuex from 'vuex'
 import * as getters from './getters'
 import * as actions from './actions'
 import * as mutations from './mutations'
+import counter from './modules/counter'
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
     state: {
-        counter: 0,
         value: 0
     },
     getters,
     mutations,
-    actions
+    actions,
+    modules: {
+        counter
+    }
 })
